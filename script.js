5319cd78da325ed29494eb9ef831a07ce0aacd92
shiva170414 <shivanand.2125csme@kiet.edu>
2025-05-09 20:17:33 +0530

    changes1

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to display the photo and hide the button
+function showPhoto() {
+  const photo = document.getElementById("myPhoto");
+  const button = document.getElementById("showBtn");
+
+  photo.style.display = "block";  // Display the image
+  button.style.display = "none";  // Hide the button
+}
